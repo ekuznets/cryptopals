cbf9ab68a4853556bd309f3c96c9d62dbcf48e17
ekuznets <ekuznets@outlook.com>
2023-06-25 21:09:49 -0700

    Solving problem 3

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rustc libpal/pal.rs --crate-type=rlib
 rustc set1/exercise1.rs --extern libpal=libpal.rlib -o e1.exe
 rustc set1/exercise2.rs --extern libpal=libpal.rlib -o e2.exe
+rustc set1/exercise3.rs --extern libpal=libpal.rlib -o e3.exe
