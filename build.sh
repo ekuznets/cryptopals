f1967f393701e3b1f22467e5e2aa68e31499eec7
ekuznets <ekuznets@outlook.com>
2023-07-10 15:58:33 -0700

    Improve build script

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
+if [ $# -eq 0 ]; then
+	echo "Usage: ./build.sh [exercise number]"
+	echo "If no argument is given, only the library will be built."
+fi
+
+# Clean library if it exists
+if [ -f libpal.rlib ]; then
+	rm libpal.rlib
+fi
+
 rustc libpal/pal.rs --crate-type=rlib
-#rustc set1/exercise1.rs --extern libpal=libpal.rlib -o e1.exe
-#rustc set1/exercise2.rs --extern libpal=libpal.rlib -o e2.exe
-#rustc set1/exercise3.rs --extern libpal=libpal.rlib -o e3.exe
-#rustc set1/exercise4.rs --extern libpal=libpal.rlib -o e4.exe
-rustc set1/exercise5.rs --extern libpal=libpal.rlib -o e5.exe
+
+# Build and run exercise if argument is given
+if [ $# -ne 0 ]; then
+	rm e$1.exe
+	rustc set1/exercise$1.rs --extern libpal=libpal.rlib -o e$1.exe
+	./e$1.exe
+fi
